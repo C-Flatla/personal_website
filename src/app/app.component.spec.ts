bfb7e602dbf3f729283025351615aa13e5ea0ee8
Cody Flatla <codyflatla@outlook.com>
2018-06-05 22:16:16 -0600

    Clean up landing page and add construction message

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -8,20 +8,36 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
+
   it(`should have as title 'app'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('codyflatla.ca');
   }));
+
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to personal-website!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to codyflatla.ca!');
+  }));
+
+  it('should have the proper construction message', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.constructionMessage).toContain('under construction');
+  }));
+
+  it('should display the construction message in a h2 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h2').textContent).toContain('under construction');
   }));
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'codyflatla.ca';
+  constructionMessage = 'This site is currently under construction';
 }
