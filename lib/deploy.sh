ceb929a92baa91a45cd8f85bd794c2b8edc4a46e
Cody Flatla <codyflatla@outlook.com>
2018-06-18 23:46:01 -0600

    Update the deploy script to have better spacing

--- lib/deploy.sh
+++ lib/deploy.sh
@@ -1,26 +1,26 @@
 # Instead of git pull: https://stackoverflow.com/a/15316602
 printf "STARTING git pull\n"
 git remote update -p; git merge --ff-only @{u}
-printf "FINISHED git pull\n"
+printf "FINISHED git pull\n\n"
 
 # Update packages
 printf "STARTING npm install\n"
 npm install
-printf "FINISHED npm install\n"
+printf "FINISHED npm install\n\n"
 
 # Build the files to be served
 printf "STARTING ng build\n"
 ng build --prod
-printf "FINISHED ng build\n"
+printf "FINISHED ng build\n\n"
 
 # Remove the old version
 printf "STARTING rm old version\n"
 rm /var/www/codyflatla.ca/html/*
-printf "FINISHED rm old version\n"
+printf "FINISHED rm old version\n\n"
 
 # Copy the new version
 printf "STARTING cp new version\n"
 cp ./dist/personal-website/* /var/www/codyflatla.ca/html/
-printf "FINISHED cp new version\n"
+printf "FINISHED cp new version\n\n"
 
-printf "ALL DONE!! YAHOO!"
+printf "ALL DONE!! YAHOO!\n"
