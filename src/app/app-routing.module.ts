24b36773ec44727f5a1899c9d5de2cc1aaddc43a
Cody Flatla <codyflatla@outlook.com>
2018-07-09 23:01:02 -0600

    Change default route to be '/'
    Instead of '/under_construction'
    Change 'under_construction' to 'under-construction' to match naming conventions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { UnderConstructionComponent } from './under-construction/under-construction.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/under_construction', pathMatch: 'full' },
-  { path: 'under_construction', component: UnderConstructionComponent }
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'under-construction', component: UnderConstructionComponent }
 ];
 
 @NgModule({
