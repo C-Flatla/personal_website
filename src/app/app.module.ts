36db5cf1a5916ce52502e8ba3d5d30d0119ea604
Cody Flatla <codyflatla@outlook.com>
2018-07-10 00:27:50 -0600

    Remove non-existent component in app.module
    Dead code left behind when renaming construction component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { ConstructionComponent } from './construction/construction.component';
 import { UnderConstructionComponent } from './under-construction/under-construction.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { InterestListComponent } from './interest-list/interest-list.component';
