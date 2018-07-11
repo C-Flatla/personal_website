62fb043f710397f53069a036fad020b3703a7dce
Cody Flatla <codyflatla@outlook.com>
2018-07-10 23:52:59 -0600

    Add route for project-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UnderConstructionComponent } from './under-construction/under-construction.component';
 import { InterestListComponent } from './interest-list/interest-list.component';
+import { ProjectListComponent } from './project-list/project-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'under-construction', component: UnderConstructionComponent },
-  { path: 'interest-list', component: InterestListComponent }
+  { path: 'interest-list', component: InterestListComponent },
+  { path: 'project-list', component: ProjectListComponent }
 ];
 
 @NgModule({
