1b0c48b3d6d67d4bdfc38f3f6e54a4c867d90eb2
Cody Flatla <codyflatla@outlook.com>
2019-07-28 16:41:32 -0600

    Import changelog data into changelog component

--- src/app/changelog/changelog.component.ts
+++ src/app/changelog/changelog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import changelogData from 'src/assets/changelog-data.json';
 
 @Component({
   selector: 'app-changelog',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./changelog.component.css']
 })
 export class ChangelogComponent implements OnInit {
+  changelogData = changelogData;
 
   constructor() { }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() { }
 }
