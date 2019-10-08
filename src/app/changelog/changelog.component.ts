ae5fbe10e94892b12fec8e5df596ef4a23a802ef
Cody Flatla <codyflatla@outlook.com>
2019-10-07 19:28:24 -0600

    Dynamically generate changelog content (#13)
    * Remove git-release-notes module and script
    
    * Add script to generate changelog
    
    * Import changelog data into changelog component
    
    * Render the changelog data on the page
    
    * Add empty changelog-data.json file
    
    To fix the tests
    
    * Change changelog script to save to assets/
    
    * Add generate-changelog to Circleci deploy job
    
    * Make sure to run checkout in deploy step
    
    * Make sure changelog json gets moved before deploy
    
    * Fix path in changelog component
    
    * Add missing compiler option
    
    * Move generate changelog to build step
    
    * Generate changelog after angular-build
    
    * Add npm script to generate changelog
    
    * Add njson-typings file
    
    In the hopes of fixing this showing up on production
    
    * Run generate changelog step before build step
    
    * Test deploy step on this branch
    
    Todo: switch this back to master
    
    * Revert 'Add njson-typings file'
    
    This reverts commit ec9873929795ce021e1e3490f1666befef084e63.
    
    * Switch deploy job to run off master

--- src/app/changelog/changelog.component.ts
+++ src/app/changelog/changelog.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import changelogData from '../../assets/changelog-data.json';
 
 @Component({
   selector: 'app-changelog',
   templateUrl: './changelog.component.html',
   styleUrls: ['./changelog.component.css']
 })
+
 export class ChangelogComponent implements OnInit {
+  changelogData: any = changelogData;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.changelogData.forEach((entry: any) => {
+      entry.date = new Date(entry.date).toLocaleString();
+      entry.commitUrl = `https://github.com/C-Flatla/personal_website/commit/${entry.commit}`;
+      entry.subject = entry.subject.replace(/\(#(\d*)\)/gm, '(<a href="https://github.com/C-Flatla/personal_website/pull/$1">#$1</a>)');
+    });
   }
 
+  ngOnInit() { }
 }
